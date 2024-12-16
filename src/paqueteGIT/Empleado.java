772231c18ce74a41e04986ca61e515faf57c6c15
Alex garai <alex.garai@somorrostro.com>
2024-12-16 08:51:41 +0100

    Añadido constructor completo

--- src/paqueteGIT/Empleado.java
+++ src/paqueteGIT/Empleado.java
@@ -1,10 +1,27 @@
 package paqueteGIT;
 
 public class Empleado {
+
 	private String dni;
 	private String nombre;
 	private String apellidos;
 	private byte edad;
 	private float sueldo;
 	private boolean hijos; //True si tiene hijos/as false en caso contrario
+	
+	/////////// CONSTRUCTORES /////////
+	public Empleado(String dni, String nombre, String apellidos, byte edad, float sueldo, boolean hijos) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.sueldo = sueldo;
+		this.hijos = hijos;
+	}
+	
+	
+	
 }
+
+
